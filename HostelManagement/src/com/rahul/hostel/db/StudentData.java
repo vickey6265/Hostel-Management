0545a708a416bc2ea6db0fc2af781e978eeec3f8
rahul makvana <rmakwana99@gmail.com>
2016-10-11 13:29:52 +0530

    save data to student

--- HostelManagement/src/com/rahul/hostel/db/StudentData.java
+++ HostelManagement/src/com/rahul/hostel/db/StudentData.java
@@ -2,6 +2,8 @@ package com.rahul.hostel.db;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,5 +43,77 @@ public class StudentData {
 			}
 		}
 		return null;
+	}
+	
+	public static int saveStudent(int sid,String name, Date dob, int age, String collage, String address, long phoneNo,
+			String email, String password, Date dateOfJoin, Date dateOfLeave, String photo,int rid,int roomNo,
+	double fees) {
+	
+		int status=0;
+		
+		
+		String sqlqry="insert into student() values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		try {
+			Connection con=Connector.getConnection();
+			
+			PreparedStatement ps=con.prepareStatement(sqlqry);
+			ps.setInt(1, sid);
+			ps.setString(2, name);
+			ps.setDate(3, dob);
+			ps.setString(4, collage);
+			ps.setString(5, address);
+			ps.setLong(6, phoneNo);
+			ps.setString(7, email);
+			ps.setString(8, password);
+			ps.setDate(9, dateOfJoin);
+			ps.setDate(10, dateOfLeave);
+			ps.setString(11, photo);
+			ps.setInt(12, rid);
+			ps.setInt(13, roomNo);
+			ps.setDouble(14, fees);
+			
+			status=ps.executeUpdate();
+		} catch (ClassNotFoundException | IOException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return status;
+	}
+	
+	public static int saveStudent_lite(int sid,String name, Date dob, int age, String collage, String address, long phoneNo,
+			String email, String password, Date dateOfJoin, Date dateOfLeave, String photo,int rid,int roomNo,
+	double fees) {
+	
+		int status=0;
+		
+		
+		String sqlqry="insert into student_lite() values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		try {
+			Connection con=Connector.getConnection();
+			
+			PreparedStatement ps=con.prepareStatement(sqlqry);
+			ps.setInt(1, sid);
+			ps.setString(2, name);
+			ps.setDate(3, dob);
+			ps.setString(4, collage);
+			ps.setString(5, address);
+			ps.setLong(6, phoneNo);
+			ps.setString(7, email);
+			ps.setString(8, password);
+			ps.setDate(9, dateOfJoin);
+			ps.setDate(10, dateOfLeave);
+			ps.setString(11, photo);
+			ps.setInt(12, rid);
+			ps.setInt(13, roomNo);
+			ps.setDouble(14, fees);
+			
+			status=ps.executeUpdate();
+		} catch (ClassNotFoundException | IOException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return status;
 	}
 }
